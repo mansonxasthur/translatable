29f0e50a415087cb4127c9d1f0cb67b82790ed2d
Manson Xasthur <mansonx13@gmail.com>
2019-10-11 20:41:14 +0200

    improvements

--- src/TranslatableServiceProvider.php
+++ src/TranslatableServiceProvider.php
@@ -30,6 +30,8 @@ class TranslatableServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->app['router']->aliasMiddleware('local', \MX13\Translatable\Http\Middleware\Locale::class);
+
         $this->loadRoutesFrom(__DIR__.'/routes/routes.php');
          $this->publishes([
              __DIR__. '/config/translatable.php' => config_path('translatable.php'),
